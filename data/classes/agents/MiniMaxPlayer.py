961d17b383ccc2b66e250478ee319e09c86f5272
juanher935 <jhernandez387@horizon.csueastbay.edu>
2024-10-16 11:28:45 -0700

    Added initial values for pieces and started minimax algorithm.

--- data/classes/agents/MiniMaxPlayer.py
+++ data/classes/agents/MiniMaxPlayer.py
@@ -1,4 +1,6 @@
 # /* MiniMaxPlayer.py
+# 14:35 For simple MinMax algorithm Example in
+# https://www.youtube.com/watch?v=IDx9iWqDwZE&list=PLBwF487qi8MGU81nDGaeNE1EnNEPYWKY_&index=12&ab_channel=EddieSharick%28Eddie%29
 
 import random
 
@@ -7,6 +9,29 @@ from data.classes.Board import Board
 from data.classes.agents.ChessAgent import ChessAgent
 
 class MiniMaxPlayer(ChessAgent):
+
+    def evaluate(self, board):
+        piece_values = {
+            'P': 1, 'N': 3, 'B': 3, 'R': 5, 'Q': 9, 'K': 0
+        }
+        score = 0
+        for row in board:
+            for piece in row:
+                if piece:
+                    value = piece_values[piece[1]]
+                    score += value if piece[0] == 'w' else -value
+        return score
+
+    def minimax(self, board, depth, maximizingPlayer):
+        if depth == 0:
+            return self.evaluate(board)
+        if maximizingPlayer:
+            max_Value = float('-inf')
+            pass
+        else:
+            pass
+            # minPlayer
+
     def choose_action(self, board: Board):
         possible_moves: list[tuple[Square, Square]] = []
 
